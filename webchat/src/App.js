63eafd52a87a25d898deab2b3af52e775f0eee55
Wei Zhang <zw83133883@yahoo.com>
2019-04-15 19:08:12 -0400

    Modified messsages component

--- webchat/src/App.js
+++ webchat/src/App.js
@@ -28,7 +28,8 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-    {this.state.user ? (<Home />) : (<Login/>)}
+          {this.state.user ? (<Home />) : (<Login/>)}
+          
       </div>
     );
   }
